c7c6d9e935a472ec4de63688dda512ba5a455293
themaximalbeing@gmail.com <themaximalbeing@gmail.com>
2023-09-12 21:55:43 +0930

    optimize flush BO and numbers

--- buildOrder.py
+++ buildOrder.py
@@ -39,50 +39,71 @@ buildOrders["FF-SCOUT-FLUSH"] =  [
   [19.40, "house", "place"],
 
   # farms (20), should be 2 reseeds
-  [5.25, "farm", "place"],
-  [7.00, "farm", "place"],
-  [12.10, "farm", "place"],
-  [12.30, "farm", "place"],
-  [12.50, "farm", "place"],
-  [13.00, "farm", "place"],
-  [14.10, "farm", "place"],
-  [14.35, "farm", "place"],
+  # [5.25, "farm", "place"],
+  # [7.00, "farm", "place"],
+
+  [11.15, "farm", "place"],
+  # [11.30, "farm", "place"],
+  # [11.55, "farm", "place"],
+  [11.20, "farm", "place"],
+
+  [11.40, "farm", "place"],
+  [12.00, "farm", "place"],
+  
+  [12.40, "farm", "place"],
+  [13.40, "farm", "place"],
+  [14.50, "farm", "place"],
+  [15.20, "farm", "place"],
   [16.05, "farm", "place"],
   [16.30, "farm", "place"],
   [16.45, "farm", "place"],
+
+
+  [17.00, "farm", "place"],
+
   [18.00, "farm", "place"],
   [18.10, "farm", "place"],
   [18.30, "farm", "place"],
   [18.40, "farm", "place"],
   [18.45, "farm", "place"],
   [19.05, "farm", "place"],
-  [19.15, "farm", "place"],
-  [19.45, "farm", "place"],
-  [20.00, "farm", "place"],
+  # [19.15, "farm", "place"],
+  # [19.45, "farm", "place"],
+  # [20.00, "farm", "place"],
 
   # buildings
   [3.00, "lumber-camp",    "place"],
   [4.20, "mill",           "place"],
   [6.30, "lumber-camp",    "place"],
-  [8.50, "mining-camp",    "place"],
+  [8.40, "mining-camp",    "place"],
+  # [8.50, "mining-camp",    "place"],
+
+
   [9.10, "barracks",       "place"],
   [10.30, "archery-range", "place"],
   [10.30, "archery-range", "place"],
-  [15.30, "blacksmith",    "place"],
+  [13.50, "blacksmith",    "place"],
 
   # units
   [0.00, "villager",   "on"],
   [0.00, "archer",     "on"],
+  [0.00, "archer",     "on"],
+  [0.00, "skirmisher", "on"],
   [0.00, "skirmisher", "on"],
 
   # techs
-  [7.04, "loom",           "on"],
+  # [7.04, "loom",           "on"],
+  # [7.54, "feudal-age",      "on"],
+
+  [7.29, "loom",           "on"],
+  [8.19, "feudal-age",      "on"],
+
+
   [0.00, "fletching",       "on"],
-  [17.00, "wheelbarrow",    "on"],
-  [7.54, "feudal-age",      "on"],
+  [16.20, "wheelbarrow",    "on"],
   [18.00, "castle-age",     "on"],
   [10.30, "double-bit-axe", "pending"],
-  [10.30, "horse-collar",   "pending"],
+  # [10.30, "horse-collar",   "pending"],
 
 ]
 
